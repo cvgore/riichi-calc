e549a4e8bcfedec4f58f19c49b7bde1577e4a5ff
Kacper Palka <kacper@cvgo.re>
2021-05-11 21:51:42 +0200

    iface fixes

--- MainWnd.Designer.cs
+++ MainWnd.Designer.cs
@@ -33,6 +33,12 @@ namespace RiichiCalc
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.Label label1;
+            System.Windows.Forms.Label label2;
+            System.Windows.Forms.Label label3;
+            System.Windows.Forms.Label label4;
+            System.Windows.Forms.Panel panel1;
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.mahjongTileBtn1 = new RiichiCalc.Controls.MahjongTileBtn();
@@ -76,16 +82,84 @@ namespace RiichiCalc
             this.button4 = new System.Windows.Forms.Button();
             this.windSelBtn1 = new RiichiCalc.Controls.WindSelBtn();
             this.windSelBtn2 = new RiichiCalc.Controls.WindSelBtn();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.winMethodEnumBtn1 = new RiichiCalc.Controls.WinMethodEnumBtn();
             this.betYakuEnumBtn1 = new RiichiCalc.Controls.BetYakuEnumBtn();
             this.extraYakuEnumBtn1 = new RiichiCalc.Controls.ExtraYakuEnumBtn();
-            this.label3 = new System.Windows.Forms.Label();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            label4 = new System.Windows.Forms.Label();
+            panel1 = new System.Windows.Forms.Panel();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.tableLayoutPanel3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            label1.Location = new System.Drawing.Point(3, 171);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(91, 59);
+            label1.TabIndex = 7;
+            label1.Text = "Table wind";
+            label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            label2.Location = new System.Drawing.Point(197, 171);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(91, 59);
+            label2.TabIndex = 8;
+            label2.Text = "Seat wind";
+            label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            label3.Location = new System.Drawing.Point(197, 57);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(91, 57);
+            label3.TabIndex = 12;
+            label3.Text = "Extra yaku";
+            label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            label4.Location = new System.Drawing.Point(197, 114);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(91, 57);
+            label4.TabIndex = 13;
+            label4.Text = "Dora";
+            label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(this.numericUpDown1);
+            panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            panel1.Location = new System.Drawing.Point(291, 114);
+            panel1.Margin = new System.Windows.Forms.Padding(0);
+            panel1.Name = "panel1";
+            panel1.Size = new System.Drawing.Size(99, 57);
+            panel1.TabIndex = 14;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Location = new System.Drawing.Point(3, 17);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(93, 23);
+            this.numericUpDown1.TabIndex = 0;
             // 
             // tableLayoutPanel1
             // 
@@ -478,12 +552,14 @@ namespace RiichiCalc
             this.tableLayoutPanel3.Controls.Add(this.button4, 3, 0);
             this.tableLayoutPanel3.Controls.Add(this.windSelBtn1, 1, 3);
             this.tableLayoutPanel3.Controls.Add(this.windSelBtn2, 3, 3);
-            this.tableLayoutPanel3.Controls.Add(this.label1, 0, 3);
-            this.tableLayoutPanel3.Controls.Add(this.label2, 2, 3);
+            this.tableLayoutPanel3.Controls.Add(label1, 0, 3);
+            this.tableLayoutPanel3.Controls.Add(label2, 2, 3);
             this.tableLayoutPanel3.Controls.Add(this.winMethodEnumBtn1, 0, 1);
             this.tableLayoutPanel3.Controls.Add(this.betYakuEnumBtn1, 1, 1);
             this.tableLayoutPanel3.Controls.Add(this.extraYakuEnumBtn1, 3, 1);
-            this.tableLayoutPanel3.Controls.Add(this.label3, 2, 1);
+            this.tableLayoutPanel3.Controls.Add(label3, 2, 1);
+            this.tableLayoutPanel3.Controls.Add(label4, 2, 2);
+            this.tableLayoutPanel3.Controls.Add(panel1, 3, 2);
             this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel3.Location = new System.Drawing.Point(399, 3);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
@@ -557,31 +633,10 @@ namespace RiichiCalc
             this.windSelBtn2.TabIndex = 6;
             this.windSelBtn2.Value = RiichiCalc.Tiles.MahjongTile.WindEast;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label1.Location = new System.Drawing.Point(3, 171);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 59);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "Table wind";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label2.Location = new System.Drawing.Point(197, 171);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(91, 59);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "Seat wind";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            // 
             // winMethodEnumBtn1
             // 
             this.winMethodEnumBtn1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.winMethodEnumBtn1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.winMethodEnumBtn1.Location = new System.Drawing.Point(3, 60);
             this.winMethodEnumBtn1.Name = "winMethodEnumBtn1";
             this.winMethodEnumBtn1.Size = new System.Drawing.Size(91, 51);
@@ -591,6 +646,7 @@ namespace RiichiCalc
             // betYakuEnumBtn1
             // 
             this.betYakuEnumBtn1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.betYakuEnumBtn1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.betYakuEnumBtn1.Location = new System.Drawing.Point(100, 60);
             this.betYakuEnumBtn1.Name = "betYakuEnumBtn1";
             this.betYakuEnumBtn1.Size = new System.Drawing.Size(91, 51);
@@ -600,22 +656,12 @@ namespace RiichiCalc
             // extraYakuEnumBtn1
             // 
             this.extraYakuEnumBtn1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.extraYakuEnumBtn1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.extraYakuEnumBtn1.Location = new System.Drawing.Point(294, 60);
             this.extraYakuEnumBtn1.Name = "extraYakuEnumBtn1";
-            this.extraYakuEnumBtn1.Size = new System.Drawing.Size(91, 51);
+            this.extraYakuEnumBtn1.Size = new System.Drawing.Size(93, 51);
             this.extraYakuEnumBtn1.TabIndex = 11;
             this.extraYakuEnumBtn1.Value = RiichiCalc.Tiles.ExtraYaku.None;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label3.Location = new System.Drawing.Point(197, 57);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(91, 57);
-            this.label3.TabIndex = 12;
-            this.label3.Text = "Extra yaku";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // MainWnd
             // 
@@ -623,10 +669,13 @@ namespace RiichiCalc
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(792, 466);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimumSize = new System.Drawing.Size(720, 500);
             this.Name = "MainWnd";
             this.Text = "RiichiCalc";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel3.ResumeLayout(false);
@@ -686,6 +735,8 @@ namespace RiichiCalc
         private BetYakuEnumBtn betYakuEnumBtn1;
         private ExtraYakuEnumBtn extraYakuEnumBtn1;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
     }
 }
 
